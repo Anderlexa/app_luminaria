f6a826a91a7f96a731faca6a07e7f20b4eda5917
Anderlexa <133814480+Anderlexa@users.noreply.github.com>
2025-07-14 09:20:47 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Inicializa FaceIO con manejo de errores
   try {
     if (typeof faceIO !== 'undefined') {
-      faceio = new faceIO("fioa2e2c", {
+      faceio = new faceIO("fioaa9ea", {
         container: "#faceio-modal-container"
       });
       console.log("FaceIO inicializado correctamente");
